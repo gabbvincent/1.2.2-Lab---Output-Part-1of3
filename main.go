8850a764011ca91bdad0b490fb3795049ac127ed
Vincent Gabb <vincentgabb@gmail.com>
2020-02-05 04:18:27 +0000

    Temp and Today

--- main.go
+++ main.go
@@ -1,38 +1,24 @@
- // created by : Vincent Gabb
- // ASCII ART
- // Title : Much WOW!                           
+package main
 
- w  w              w w                w  w
- w  w            w w w w              w  w
-  w  w         w  w   w  w           w  w
-   w  w        w  w     w  w        w  w
-    w  w      w  w       w  w      w  w
-     w  w    w  w         w  w    w  w
-      w  w  w  w           w  w  w  w
-       w  w   w             w   w  w
-        w   w                w    w
-         w w                  w  w
-          w                    w
-                o o o o o
-              o           o
-            o   o o o o o   o
-            o  o         o  o
-            o  o         o  o
-            o  o         o  o
-            o   o o o o o   o
-              o            o 
-                o o o o o
+import "fmt"
 
- w  w              w w                w  w
- w  w            w w w w              w  w
-  w  w         w  w   w  w           w  w
-   w  w        w  w     w  w        w  w
-    w  w      w  w       w  w      w  w
-     w  w    w  w         w  w    w  w
-      w  w  w  w           w  w  w  w
-       w  w   w             w   w  w
-        w   w                w    w
-         w w                  w  w
-          w                    w
+func main() {
+  var week string = "Tuesday the Second."
+  var month string = "Febuary,"
+  var day , month2 , year int
+  day = 2
+  month2 = 4
+  year = 2020
+  var town string = "Chico"
+  var today float64 = 64.3
+  var tomorrow float64 = 78.8
 
+  fmt.Println("Hello!, Today's date is" , month , week)
+
+  fmt.Println(day , "/" , month2 , "/" , year)
+
+  fmt.Println("We're here in" , town , "California where it is" , today , "degrees outside. Tomorrow we are looking at a significant rise in tempature with a high of" , tomorrow , "degrees, so go outside and enjoy the sun!")
+
+// end.
+}
                
