7a09252b4058bc8bbb74f57a6a1cac361a458e49
Vincent Gabb <vincentgabb@gmail.com>
2020-02-05 03:20:15 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+ // created by : Vincent Gabb
+ // ASCII ART
+ // Title : Much WOW!                           
+
+ w  w              w w                w  w
+ w  w            w w w w              w  w
+  w  w         w  w   w  w           w  w
+   w  w        w  w     w  w        w  w
+    w  w      w  w       w  w      w  w
+     w  w    w  w         w  w    w  w
+      w  w  w  w           w  w  w  w
+       w  w   w             w   w  w
+        w   w                w    w
+         w w                  w  w
+          w                    w
+                o o o o o
+              o           o
+            o   o o o o o   o
+            o  o         o  o
+            o  o         o  o
+            o  o         o  o
+            o   o o o o o   o
+              o            o 
+                o o o o o
+
+ w  w              w w                w  w
+ w  w            w w w w              w  w
+  w  w         w  w   w  w           w  w
+   w  w        w  w     w  w        w  w
+    w  w      w  w       w  w      w  w
+     w  w    w  w         w  w    w  w
+      w  w  w  w           w  w  w  w
+       w  w   w             w   w  w
+        w   w                w    w
+         w w                  w  w
+          w                    w
+
+               
